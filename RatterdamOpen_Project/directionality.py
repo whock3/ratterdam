f48d2d8f2423cb9cc33f42d58819366993de10d2
ruoyahlai <66927297+ruoyahlai@users.noreply.github.com>
2021-02-15 16:06:58 -0500

    Samples are visits excluding points outside of the field. Can use one-way ANOVA instead of kruskal wallis.

--- RatterdamOpen_Project/directionality.py
+++ RatterdamOpen_Project/directionality.py
@@ -10,6 +10,9 @@ import numpy as np
 from scipy.stats import kruskal
 import csv
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
+import matplotlib.path as path
+import scipy
+from math import ceil, sqrt
 from confounds import directionFilterS
 import ratterdam_Defaults as Def
 import utility_fx as util
@@ -54,22 +57,28 @@ def graphVisitsDir(unit, subfield, title=""):
     ax.axis('equal')
             
 
-def rateDir(unit, title):
+def rateDir(unit, title, test):
     """
     Plots a bar graph of the average rate vs NESW for each subfield. Calculates
     stats using Kruskal Wallis and Tukey
+    
+    test: "kruskal" or "anova"
     """
     #calculates rates for each direction using visits as samples
     avgRate = np.empty((len(unit.perimeters), 4)) #rows=subfields, columns=NESW
     visitRates = [] #levels of lists: subfields, directions, visits
     for i in range(len(unit.perimeters)):
         visitRates.append([[], [], [], []])
+        contour = path.Path(unit.perimeters[i])
+        PinC = unit.position[contour.contains_points(unit.position[:,1:])]
+        SinC = unit.spikes[contour.contains_points(unit.spikes[:,1:])]
         for visit in range(len(unit.visits[i])):
-            visitPos = np.logical_and(unit.position[:,0] >= unit.visits[i][visit][0],
-                                      unit.position[:,0] <= unit.visits[i][visit][-1])
-            visitSpk = np.logical_and(unit.spikes[:,0] >= unit.visits[i][visit][0],
-                                      unit.spikes[:,0] <= unit.visits[i][visit][-1])
-            posDir, spikesDir = directionFilterS(unit.position[visitPos], unit.spikes[visitSpk])
+            visitPos = np.logical_and(PinC[:,0] >= unit.visits[i][visit][0],
+                                      PinC[:,0] <= unit.visits[i][visit][-1])
+            visitSpk = np.logical_and(SinC[:,0] >= unit.visits[i][visit][0],
+                                      SinC[:,0] <= unit.visits[i][visit][-1])
+            
+            posDir, spikesDir = directionFilterS(PinC[visitPos], SinC[visitSpk])
             for j in range(4):
                 if len(posDir[j]) == 0:
                     visitRate = np.nan
@@ -81,28 +90,26 @@ def rateDir(unit, title):
             avgRate[i,j] = np.nanmean(visitRates[i][j])
     
     #dimensions for subplots
-    if len(unit.perimeters) <= 2:
-        subfieldDim = [2,1]
-    elif 2 < len(unit.perimeters) <= 4:
-        subfieldDim = [2,2]
-    elif 4 < len(unit.perimeters) <= 6:
-        subfieldDim = [2,3]
-    elif 6 < len(unit.perimeters) <= 9:
-        subfieldDim = [3,3]
-    elif 9 < len(unit.perimeters) <= 12:
-        subfieldDim = [3,4]
-    elif 12 < len(unit.perimeters) <= 16:
-        subfieldDim = [4,4]
-    else:
-        subfieldDim = [5,5]
+    rows = round(sqrt(len(unit.perimeters)))
+    subfieldDim = [rows, ceil(len(unit.perimeters)/rows)]
     
     #graphing and calculating stats
     fig, axs = plt.subplots(subfieldDim[0], subfieldDim[1], figsize=(subfieldDim[1]*3,subfieldDim[0]*3))
-    axs = axs.flatten()
+    if len(unit.perimeters) > 1:
+        axs = axs.flatten()
+    else: #if there is only 1 field so only one axis for graphing
+        axs = [axs] #make axs subscriptable
     tukeys = []
     for i in range(len(visitRates)): #i = subfield number
         sig = ""
-        Hstats, p = kruskal(visitRates[i][0], visitRates[i][1], visitRates[i][2], visitRates[i][3], nan_policy="omit")
+        if test == "kruskal":
+            Hstats, p = kruskal(visitRates[i][0], visitRates[i][1], visitRates[i][2], visitRates[i][3], nan_policy="omit")
+        elif test == "anova":
+            noNan = []
+            for j in range(4):
+                noNan.append(np.array(visitRates[i][j])[~np.isnan(visitRates[i][j])])
+            f, p = scipy.stats.f_oneway(noNan[0], noNan[1], noNan[2], noNan[3])
+            
         if p < 0.05:
             sig = "*"
             groups = []
@@ -123,7 +130,7 @@ def rateDir(unit, title):
         axs[i].set_xticklabels(["N", "E", "S", "W"])
     for i in range(len(unit.perimeters), subfieldDim[0]*subfieldDim[1]):
         axs[i].axis("off")
-    fig.suptitle(title, y=1.06)
+    fig.suptitle(title, y=1.08)
     fig.tight_layout()
     
     return fig, tukeys
@@ -141,13 +148,14 @@ def readCells(file, df):
     return cells
 
 
-def rateDirs(file, title, timestamp, dfData, df2="W:/Ratterdam/R859/ratterdam_tabulations/", dfGenerated="R:/072720-/Directionality/"):
+def rateDirs(file, title, timestamp, dfData, test, df2="W:/Ratterdam/R859/ratterdam_tabulations/", dfGenerated="R:/072720-/Directionality/"):
     """
     file: file name of the file with repetition tabulations
     title: part of the title of file being generated (include rat and day)
     dfData: path to data
+    test: "kruskal" or "anova"
     df2: path to repetition tabulations
-    dfGenerates: path to where to save the generated files
+    dfGenerated: path to where to save the generated files
     
     Make rate vs direction bar graphs, directional ratemaps, and csv's of 
     directional tuning of fields
@@ -157,20 +165,20 @@ def rateDirs(file, title, timestamp, dfData, df2="W:/Ratterdam/R859/ratterdam_ta
     for cell in cells:
         cellTitle = cell.replace("\\cl-maze", " ")
         unit = loadRepeatingUnit(dfData, cell)
-        fig, tukeys = rateDir(unit, f"{title} {cellTitle}\nvthresh = {vthresh} cm/s")
+        fig, tukeys = rateDir(unit, f"{title} {cellTitle}\nvthresh = {vthresh} cm/s  {test}", test)
         
         fig.savefig(dfGenerated + f"{timestamp} - {title} {cellTitle} rate vs direction.png",
                     bbox_inches="tight")
         plt.close()
         
-        graphDirRatemaps(unit, f"{title} {cellTitle}\nvthresh = {vthresh} cm/s")
-        plt.savefig(dfGenerated + f"{timestamp} - {title} {cellTitle} directional ratemap.png",
-                    bbox_inches="tight")
-        plt.close()
+        #graphDirRatemaps(unit, f"{title} {cellTitle}\nvthresh = {vthresh} cm/s")
+        #plt.savefig(dfGenerated + f"{timestamp} - {title} {cellTitle} directional ratemap.png",
+        #            bbox_inches="tight")
+        #plt.close()
         
         with open(f"{dfGenerated}{timestamp} - {title} {cellTitle} directionality.csv", "w", newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(["Field", "K-W", "EN","ES","EW","NS","NW","SW"])
+            csvwriter.writerow(["Field", test, "EN","ES","EW","NS","NW","SW"])
             for i in range(len(unit.perimeters)):
                 if tukeys[i]:
                     csvwriter.writerow([i, True, tukeys[i].reject[0], tukeys[i].reject[1], tukeys[i].reject[2]
